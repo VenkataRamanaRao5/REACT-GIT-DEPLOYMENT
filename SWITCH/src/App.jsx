604989f77224b2ffc69660f6fd3baac0ee37d5a6
themysterysolver <arjunprabhakar1910@gmail.com>
2025-11-16 08:40:49 +0530

    INIT:Basic react page #2

---
+++ SWITCH/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        Hello this is prabha here!!
+      </div>
+    </>
+  )
+}
+
+export default App
